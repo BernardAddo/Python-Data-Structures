3c587e10e9e5541726fe5cb7bad9b7916e3d891d
Addo Bernard Tawiah <bernardaddo1@gmail.com>
2021-06-03 21:50:42 +0000

    stack class with maximum size

--- Stack/Stack Class.py
+++ Stack/Stack Class.py
@@ -1,9 +1,16 @@
 class Stack:
     def __init__(self):
+        self.maxsize = 0
         self.items = []
 
     def push(self, item):
-        self.items.append(item)
+        if self.maxsize == 0:
+            self.items.append(item)
+        else:
+            if len(self.items) != self.maxsize:
+                self.items.append(item)
+            else:
+                print("Stack full")
 
     def pop(self):
         return self.items.pop()
@@ -18,4 +25,15 @@ class Stack:
             return False
 
     def size(self):
-        return len(self.item)
+        return len(self.items)
+
+    def Maxsize(self, maxsize):
+        self.maxsize = maxsize
+
+    def Is_full(self):
+        if len(self.items) == self.maxsize:
+            return True
+
+    def __str__(self):
+        return f"{self.items}"
+
